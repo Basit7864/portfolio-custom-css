6aaa26be0406d9f93c9b6af29e9f13cec13cf468
Syeda Shanzay <syedashanzay04@gmail.com>
2024-11-10 01:54:21 +0500

    first commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import Navbar from "./Navbar";
+import '../app/styles/hero.css';
 
-// Changes: Added bg-black to fix the background color and made sure the image fits the container
 const Hero = () => {
   return (
     <div
-  id="hero"
-  className=" min-h-screen bg-no-repeat bg-[url(/banner_bg.png)] bg-cover"
-  style={{ backgroundSize: "35%", backgroundPosition: "left 100px top 100px"}} 
->
-
+      id="hero"
+      className="hero-container"
+    >
       <Navbar />
-      <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]">
-        <div className="hidden lg:block"></div>
+      <div className="hero-content">
+        <div></div>
 
-        {/* Changes: Adjusted text size and centering */}
-        <div className="text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center text-white">
-          <div>
-            <p data-aos="zoom-in-up">I'm</p>
+        <div className="hero-text">
+          <div className="hero-msin"> 
+            <p data-aos="zoom-in-up">I am</p>
             <p data-aos="zoom-in-up">Syeda</p>
             <p data-aos="zoom-in-up">Shanzay</p>
           </div>
